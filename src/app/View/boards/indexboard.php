1f0f6b11debf246a21ff47924a3542dbbd1527e5
Angélique <angelique.paquot.pro@gmail.com>
2021-09-10 14:16:26 +0200

    mise en commun

--- src/app/View/boards/indexboard.php
+++ src/app/View/boards/indexboard.php
@@ -3,16 +3,33 @@
 <?php // Use any data loaded in the controller here ?>
 <?php require '../app/View/includes/divdroite.php' ?>
 
-<section>
-    <h1>Users</h1>
-    
-    <ul>
-        <?php foreach ($users as $user) : ?>
-            
-            <?php /*<li><a href="index.php?page=articles-show&title=<?= $article["title"]?>"><?= $article["title"] ?> (<?= $article["publish_date"]?>)</a> </li>*/?>
-            <li><a href="index.php?page=Users-show&Username=<?= $user->Username?>"><?=$user->Username ?>  </a></li>
-        <?php endforeach; ?>
-    </ul>
-</section>
+<h2>Category One</h2>
+        <section class="category">
+          <article>
+            <div class="topic">
+              <img src="./img/girafe.jpeg" alt="" class="avatar" />
+              <div class="topic-info">
+                <header class="topic-header">Topic Type Demos</header>
+                <p class="topic-desc">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  neque, est fuga omnis beatae quae.
+                </p>
+              </div>
+            </div>
+            <div class="bottom-infos">
+              <div class="bottom-info">
+                <span class="count">278</span>
+                <span class="infos">topics</span>
+              </div>
+              <div class="bottom-info">
+                <span class="count">345</span>
+                <span class="infos">post</span>
+              </div>
+              <div class="bottom-info">
+                <span class="info-date">Sun Fe 03</span>
+                <span class="infos">last post</span>
+              </div>
+            </div>
+          </article>
 
 <?php require '../app/View/includes/footer.php'?>
--- src/app/View/topics/indextopic.php
+++ src/app/View/topics/indextopic.php
@@ -3,16 +3,31 @@
 <?php // Use any data loaded in the controller here ?>
 <?php require '../app/View/includes/divdroite.php' ?>
 
-<section>
-    <!-- <h1>Users</h1>
-    
-    <ul>
-        <?php foreach ($users as $user) : ?>
-            
-            <?php /*<li><a href="index.php?page=articles-show&title=<?= $article["title"]?>"><?= $article["title"] ?> (<?= $article["publish_date"]?>)</a> </li>*/?>
-            <li><a href="index.php?page=Users-show&Username=<?= $user->Username?>"><?=$user->Username ?>  </a></li>
-        <?php endforeach; ?>
-    </ul> -->
-</section>
+<article class="topic-card">
+      <div class="topic-header">
+        <img src="./src/public/img/lion.jpeg" alt="" class="topic-image" />
+        <div class="topic-info">
+          <h4 class="topic-title">Topic Type Demos</h4>
+          <p class="topic-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+      </div>
+      <div class="separation"></div>
+      <div class="card-footer">
+        <div class="bottom-info">
+          <span class="count">278</span>
+          <span class="infos">topics</span>
+        </div>
+        <div class="bottom-info">
+          <span class="count">345</span>
+          <span class="infos">post</span>
+        </div>
+        <div class="bottom-info">
+          <span class="info-date">Sun Fe 03</span>
+          <span class="infos">last post</span>
+        </div>
+      </div>
+    </article>
 
 <?php require '../app/View/includes/footer.php'?>
